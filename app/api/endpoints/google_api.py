ca23a8c13ff700b7488967414b3576bf3b5f7a9b
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2023-08-04 22:52:23 +0300

    added: google endpoint

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -30,9 +30,9 @@ async def get_report(
     projects = await charity_project_crud.get_projects_by_completion_rate(
         session
     )
-    spreadsheet_id = await spreadsheets_create(wrapper_services)
-    await set_user_permissions(spreadsheet_id, wrapper_services)
     try:
+        spreadsheet_id = await spreadsheets_create(wrapper_services)
+        await set_user_permissions(spreadsheet_id, wrapper_services)
         await spreadsheets_update_value(
             spreadsheet_id,
             projects,
